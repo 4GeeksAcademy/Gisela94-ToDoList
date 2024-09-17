1a25a6b747c1fc2e585cfbb67f7f0440553562d4
Gisela94 <gisela.garrido@hotmail.com>
2024-09-17 15:45:26 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons'; // Importar el icono de la basura
+
 
 //create your first component
-const Home = () => {
+function Home() {
+	const [tareas, setTareas] = useState([]);
+	const [nuevaTarea, setNuevaTarea] = useState("");
+
+	//Función para agregar tarea
+	const agregarTarea = (e) => {
+		if (e.key === "Enter" && nuevaTarea.trim() !== "") {
+			setTareas([...tareas, nuevaTarea]);
+			setNuevaTarea(""); //Limpiar el input
+		}
+	};
+
+	//Función para eliminar tarea por índice
+	const eliminarTarea = (index) => {
+		const nuevasTareas = tareas.filter((_, i) => i !== index);
+		setTareas(nuevasTareas);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="list">
+			<h1>todos</h1>
+			<div className="input">
+				<input type="text" placeholder="What needs to be done?"
+				value={nuevaTarea}
+				onChange={(e) => setNuevaTarea(e.target.value)} onKeyDown={agregarTarea} // Capturar el evento de "Enter"
+				 />
+			</div>
+
+			<ul style={{ listStyleType: "none", padding: 0 }}>
+				{tareas.map((tarea, index) => (
+					<li key={index} style={{ margin: "10px 0" }}>
+						{tarea}
+						<button onClick={() => eliminarTarea(index)}>
+							 <FontAwesomeIcon icon={faXmark} className="icon" /> </button></li>
+				))}
+			</ul>
+
 		</div>
+
 	);
-};
+}
 
 export default Home;
